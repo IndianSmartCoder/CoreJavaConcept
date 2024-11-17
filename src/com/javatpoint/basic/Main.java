4bd432c94332edcb43eeb78617291807640d8cad
IndianSmartCoder <ergovindray@gmail.com>
2024-11-17 16:52:58 +0530

    first programme

--- src/com/javatpoint/basic/Main.java
+++ src/com/javatpoint/basic/Main.java
@@ -1,5 +1,8 @@
+package com.javatpoint.basic;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ram Ram JI");
+        System.out.println("Har Har Mahadev");
     }
 }
